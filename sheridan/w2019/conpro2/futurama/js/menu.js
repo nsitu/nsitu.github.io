17c72ea5d7d6828e1f79a71bb5714ad8757f3ed9
Harold Sikkema <harold@nsitu.ca>
2019-03-02 17:12:10 -0500

    Updates

--- sheridan/w2019/conpro2/futurama/js/menu.js
+++ sheridan/w2019/conpro2/futurama/js/menu.js
@@ -1,30 +1,15 @@
-$(document).ready(function() {
-  //toggle menu
-  $(".hamburger-container").click(function() {
-    $("#menu").slideToggle();
-  });
-
-  //to fix issue that toggle adds style(hides) to nav
-  $(window).resize(function() {
-    if (window.innerWidth > 1024) {
-      $("#menu").removeAttr("style");
-    }
-  });
-
-  //icon animation
-  var topBar = $(".hamburger li:nth-child(1)"),
-    middleBar = $(".hamburger li:nth-child(2)"),
-    bottomBar = $(".hamburger li:nth-child(3)");
-
-  $(".hamburger-container").on("click", function() {
-    if (middleBar.hasClass("rot-45deg")) {
-      topBar.removeClass("rot45deg");
-      middleBar.removeClass("rot-45deg");
-      bottomBar.removeClass("hidden");
-    } else {
-      bottomBar.addClass("hidden");
-      topBar.addClass("rot45deg");
-      middleBar.addClass("rot-45deg");
-    }
-  });
-});
+function populateMenu(){
+  var links = {
+      "Student Name": "https://google.com",
+      "Another Student": "https://facebook.com"
+  };
+  for (var name in links) {
+      if (links.hasOwnProperty(name)) {
+        var theOption = document.createElement("option");
+        theOption.text = name;
+        theOption.value = links[name];
+        var theSelect = document.getElementById("theStudents");
+        theSelect.appendChild(theOption);
+      }
+  }
+}
