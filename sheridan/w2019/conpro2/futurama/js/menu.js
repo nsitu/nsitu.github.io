5543d851b5a10a41cb5560b6178ef20b30e7720a
Harold Sikkema <harold@nsitu.ca>
2019-03-17 15:41:47 -0400

    Update menu.js

--- sheridan/w2019/conpro2/futurama/js/menu.js
+++ sheridan/w2019/conpro2/futurama/js/menu.js
@@ -1,15 +1,16 @@
 function populateMenu(){
   var links = {
-      "Student 01": "https://google.com",
-      "Student 02": "https://facebook.com",
-      "Student 03": "https://google.com",
-      "Student 04": "https://facebook.com",
-      "Student 05": "https://google.com",
-      "Student 06": "https://facebook.com",
-      "Student 07": "https://google.com",
-      "Student 08": "https://facebook.com",
-      "Student 09": "https://google.com",
-      "Student 10": "https://facebook.com"
+"Betts, Emily": "http://firebird.sheridanc.on.ca/~ixd492/ConProWebCat2019/",
+"Finch, Marlee": "http://ixd589.firebird.sheridanc.on.ca/ConProWebCat/",
+"Fulkerson, Jessica": "http://ixd590.firebird.sheridanc.on.ca/ConProWebCat2019/",
+"Lillo Meza, Michelle": "http://ixd564.firebird.sheridanc.on.ca/ConPro_WebCatalogue/",
+"Luk, William": "http://ixd541.firebird.sheridanc.on.ca/Futurama/",
+"Mcghie, Faith": "http://www.firebird.sheridanc.on.ca/~ixd507/futurama/",
+"Morabito, Carina": "https://firebird.sheridanc.on.ca/~ixd508/conpro_project2/",
+"Tang, Xinyue": "http://firebird.sheridanc.on.ca/~ixd548/ConProWebCat2019/",
+"Xiao, Zhendong": "http://ixd549.firebird.sheridanc.on.ca/process2/",
+"Zhang, Di": "http://firebird.sheridanc.on.ca/~ixd550/conproweb2019/",
+"Zhi, Xiaotong": "http://firebird.sheridanc.on.ca/~ixd551/futurama/"
   };
   for (var name in links) {
       if (links.hasOwnProperty(name)) {
