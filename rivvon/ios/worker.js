da7748b722225db125dd747ce774a479f69e07df
Harold Sikkema <harold@nsitu.ca>
2025-06-06 10:31:14 -0300

    add logging to ios variant

--- rivvon/ios/worker.js
+++ rivvon/ios/worker.js
@@ -1,20 +1,29 @@
-// worker.js (ESM format, no importScripts)
-
 onmessage = async ({ data: { track } }) => {
-  const vtg = new VideoTrackGenerator();
-  const processor = new MediaStreamTrackProcessor({ track });
-  const { readable } = processor;
+  console.log('[Worker] Received track:', track);
 
-  const transformer = new TransformStream({
-    async transform(videoFrame, controller) {
-      // no-op passthrough (could apply effects here)
-      controller.enqueue(videoFrame);
-    }
-  });
+  try {
+    const vtg = new VideoTrackGenerator();
+    const processor = new MediaStreamTrackProcessor({ track });
+    const { readable } = processor;
 
-  readable
-    .pipeThrough(transformer)
-    .pipeTo(vtg.writable);
+    console.log('[Worker] Initialized processor and generator');
 
-  postMessage({ track: vtg.track }, [vtg.track]);
+    const transformer = new TransformStream({
+      async transform(videoFrame, controller) {
+        console.log('[Worker] Frame received:', videoFrame);
+        controller.enqueue(videoFrame);
+      }
+    });
+
+    readable
+      .pipeThrough(transformer)
+      .pipeTo(vtg.writable)
+      .then(() => console.log('[Worker] Pipeline complete'))
+      .catch(err => console.error('[Worker] Pipeline error:', err));
+
+    postMessage({ track: vtg.track }, [vtg.track]);
+    console.log('[Worker] Sent processed track back to main thread');
+  } catch (err) {
+    console.error('[Worker] Error initializing pipeline:', err);
+  }
 };
